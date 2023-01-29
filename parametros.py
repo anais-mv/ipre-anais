cf3e46cc8d9aa3c7ba610f6fdef7cdad9d9492e2
ANAIS MONTANARES <aamontanares@uc.cl>
2023-01-29 16:26:28 -0300

    avance

--- parametros.py
+++ parametros.py
@@ -1,11 +1,11 @@
 # máximo y mínimo número de proposiciones existentes posibles
-max_prop = 20
-min_prop = 15
+max_prop = 100
+min_prop = 50
 
 # máximo y mínimo número de operadores existentes posibles
-max_operadores = 30
-min_operadores = 15
+max_operadores = 200
+min_operadores = 100
 
 rango = 3
 
-can_del = 3
+can_add = 5
