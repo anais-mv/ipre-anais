7f28bf731f52d814498d7302be55d7f4138613df
ANAIS MONTANARES <aamontanares@uc.cl>
2023-01-29 16:16:09 -0300

    avance

--- operadores.py
+++ operadores.py
@@ -2,7 +2,7 @@ import random
 from copy import copy
 from clases import Operador
 import numpy as np
-from parametros import min_prop, max_prop, min_operadores, max_operadores, rango, can_del
+from parametros import min_prop, max_prop, min_operadores, max_operadores, rango, can_add
 
 can_prop = random.randint(min_prop, max_prop)
 can_operadores = random.randint(min_operadores, max_operadores)
@@ -44,13 +44,11 @@ for i in range(0, can_operadores):
     can_prec = random.randint(1, can_prop - rango)
     prec, prop_sin_prec = crear_prec(can_prec, prop_disponibles)
     # print("cantidad de prop disp: " + str(len(prop_disponibles)))
-    can_add = random.randint(0, len(prop_sin_prec))  # CAMBIAR CANTIDAD DE PROPOSICIONES EN ADD(?)
+    # can_add = random.randint(0, len(prop_sin_prec))  # CAMBIAR CANTIDAD DE PROPOSICIONES EN ADD(?)
+    if can_add > len(prop_sin_prec):
+        can_add = random.randint(0, len(prop_sin_prec))
     add = crear_add(can_add, prop_sin_prec)
-    if can_del > can_prec:
-        can_del = random.randint(0, can_prec)
-    # else:
-    #     can_del = can_delet
-    # can_del = random.randint(0, can_prec)  # esto es porque del debe ser un subconjunto de prec
+    can_del = random.randint(0, can_prec)  # esto es porque del debe ser un subconjunto de prec
     delet = crear_del(can_del, list(prec))  # solo escogemos proposiciones que estén en prec
     operador = Operador(i, prec, add, delet)
     operadores_disponibles.add(operador)
